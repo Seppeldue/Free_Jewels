6f66595f0724d74abbd8a1eada738a3ac23a9a6c
Seppeldue <seppeldue@tuta.io>
2020-03-06 12:37:50 +0100

    init

---
+++ FJ_IterativeBoolDif/FJ_IterativeBoolDif/FJIterativeBoolDifCommand.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using Rhino;
+using Rhino.Commands;
+using Rhino.Geometry;
+using Rhino.Input;
+using Rhino.Input.Custom;
+
+namespace FJ_IterativeBoolDif
+{
+    public class FJIterativeBoolDifCommand : Command
+    {
+        public FJIterativeBoolDifCommand()
+        {
+            // Rhino only creates one instance of each command class defined in a
+            // plug-in, so it is safe to store a refence in a static property.
+            Instance = this;
+        }
+
+        ///<summary>The only instance of this command.</summary>
+        public static FJIterativeBoolDifCommand Instance
+        {
+            get; private set;
+        }
+
+        ///<returns>The command name as it appears on the Rhino command line.</returns>
+        public override string EnglishName
+        {
+            get { return "FJ_IterativeBoolDif"; }
+        }
+
+        protected override Result RunCommand(RhinoDoc doc, RunMode mode)
+        {
+
+            Rhino.DocObjects.ObjRef objref;
+            Result rc = Rhino.Input.RhinoGet.GetOneObject("Select polysurface to subtract from",
+                                                                false, Rhino.DocObjects.ObjectType.PolysrfFilter, out objref);
+            if (rc != Rhino.Commands.Result.Success || objref == null)
+                return rc;
+            if (objref == null)
+                return Rhino.Commands.Result.Failure;
+
+            Rhino.Geometry.Brep brep = objref.Brep();
+            Guid firstBrep = objref.ObjectId;
+            doc.Objects.UnselectAll();
+            Rhino.DocObjects.ObjRef[] objrefs;
+            Result rcd = Rhino.Input.RhinoGet.GetMultipleObjects("Select set of polysurfaces to subtract",
+              false, Rhino.DocObjects.ObjectType.PolysrfFilter, out objrefs);
+            if (rcd != Rhino.Commands.Result.Success)
+                return rcd;
+            if (objrefs == null || objrefs.Length < 1)
+                return Rhino.Commands.Result.Failure;
+
+            List<Rhino.Geometry.Brep> breps = new List<Rhino.Geometry.Brep>();
+            for (int i = 0; i < objrefs.Length; i++)
+            {
+                Rhino.Geometry.Brep brepd = objrefs[i].Brep();
+                Guid secondBrep = objrefs[i].ObjectId;
+                if (brepd != null && firstBrep != secondBrep)
+                    breps.Add(brepd);
+            }
+            doc.Objects.UnselectAll();
+            string name_fail_layer = "FJ Boolean Fails";
+            Rhino.DocObjects.Layer boolFail = new Rhino.DocObjects.Layer();
+            boolFail.Name = name_fail_layer;
+            boolFail.Color = System.Drawing.Color.Red;
+            doc.Layers.Add(boolFail);
+            var fail_layer_index = doc.Layers.FindName(name_fail_layer);
+
+            string name_done_layer = "FJ Boolean Done";
+            Rhino.DocObjects.Layer boolDone = new Rhino.DocObjects.Layer();
+            boolDone.Name = name_done_layer;
+            boolDone.Color = System.Drawing.Color.BlueViolet;
+            doc.Layers.Add(boolDone);
+            var done_layer_index = doc.Layers.FindName(name_done_layer);
+
+            double tolerance = doc.ModelAbsoluteTolerance;
+            int a = 0;
+            for (int i = 0; i < breps.Count; i++)
+            {
+                RhinoApp.WriteLine("computing number: " + i + " of: " + breps.Count + " operations...");
+                try
+                {
+                    Rhino.Geometry.Brep[] brepDif = Rhino.Geometry.Brep.CreateBooleanDifference(brep, breps[i], tolerance, true);
+                    brep = brepDif[0];
+
+                }
+                catch
+                {
+                    a++;
+                    Rhino.DocObjects.RhinoObject objFail = objrefs[i].Object();
+                    objFail.Attributes.LayerIndex = fail_layer_index.Index;
+                    objFail.CommitChanges();
+                    continue;
+                }
+                Rhino.DocObjects.RhinoObject obj2Org = objrefs[i].Object();
+                obj2Org.Attributes.LayerIndex = done_layer_index.Index;
+                obj2Org.CommitChanges();
+                //doc.Objects.Delete(obj2Org);
+                doc.Views.Redraw();
+            }
+
+            RhinoApp.WriteLine(a + " of " + breps.Count + " operations failed!");
+            Rhino.DocObjects.RhinoObject obj1Org = objref.Object();
+            doc.Objects.Delete(obj1Org);
+
+            doc.Objects.AddBrep(brep);
+
+            doc.Views.Redraw();
+            return Rhino.Commands.Result.Success;
+
+
+        }
+    }
+}
